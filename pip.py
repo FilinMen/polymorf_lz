4d0a02c54a450cddbdd462e2338b8cd348d503ad
FilinMen <fiilinkadabra@tutamail.com>
2025-04-21 15:57:22 +0300

    sda

---
+++ pip.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def data():
+    df = pd.read_csv('var3.csv')
+    
+
+    df1 = df[df['Сумма операции'] < 1000]    # строки, удовлетворяющие условию
+    df2 = df[df['Сумма операции'] >= 1000]    # строки, не удовлетворяющие условию
+
+    df1.to_csv('file1.csv') #до 1000
+    df2.to_csv('file2.csv') # больше 1000
+
+    duplicates1 = df1.duplicated()
+    duplicates2 = df2.duplicated()
+
+    # подсчет дубликатов
+    num_duplicates1 = duplicates1.sum()
+    num_duplicates2 = duplicates2.sum()
+    num = num_duplicates1 + num_duplicates2
+
+    print(num)
+
+data()
